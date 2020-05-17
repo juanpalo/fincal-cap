bdd9529fda94a240c0ec5fdd2dd9b95df5025980
juancasas <juancasas1008@gmail.com>
2020-05-16 20:03:35 -0700

    vh and vh being taken up, header needs to be turned into a link to home page

--- src/components/headernavbar/header.js
+++ src/components/headernavbar/header.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 
 
+
 class Header extends Component {
     render() {
         return (
             <div className="header">
-                <img src='http://via.placeholder.com/75x75'/>
+                <h1>GEN EMPIRE</h1>
             </div>
         )
     }
